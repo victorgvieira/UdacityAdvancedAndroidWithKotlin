32800a9850d5c6fc2aa9b15f160bdc876604632d
victorgvieira <vggvieira@gmail.com>
2019-11-20 00:37:10 -0300

    :sparkle: Create the MyCanvasView Class

---
+++ DrawingOnCanvasObjects/MiniPaint/app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -0,0 +1,9 @@
+package com.example.android.minipaint
+
+import android.content.Context
+import android.view.View
+
+// DONE Step: 2.0 create the file MyCanvasView
+// DONE Step: 2.1 make MyCanvasView class extends View class passing the context
+// DONE Step: 2.2 follow sugested imports
+class MyCanvasView(context: Context) : View(context) {}

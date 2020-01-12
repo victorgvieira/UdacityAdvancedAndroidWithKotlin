d8d40771f7fc78aea4b547e8b5cb7d610d1c75f9
victorgvieira <vggvieira@gmail.com>
2020-01-12 04:37:06 -0300

    :sparkle: Update Broadcast Receiver

--- VirtualTreasureHuntWithGeofences/android-kotlin-geo-fences/app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
+++ VirtualTreasureHuntWithGeofences/android-kotlin-geo-fences/app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
@@ -22,7 +22,6 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.content.ContextCompat
-import com.example.android.treasureHunt.HuntMainActivity.Companion.ACTION_GEOFENCE_EVENT
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
 
@@ -37,7 +36,51 @@ import com.google.android.gms.location.GeofencingEvent
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // TODO: Step 11 implement the onReceive method
+        // DONE: Step 11 implement the onReceive method
+        // DONE: Step 11.1 Check that the intent’s action is of type ACTION_GEOFENCE_EVENT
+        if (intent.action == HuntMainActivity.ACTION_GEOFENCE_EVENT) {
+            // DONE: Step 11.2 Create a variable called geofencingEvent and initialize it to GeofencingEvent with the intent passed in
+            val geofencingEvent = GeofencingEvent.fromIntent(intent)
+            // DONE: Step 11.3 In the case that there is an error, you will want to understand what went wrong.
+            //  Save a variable with the error message obtained through the geofences error code.
+            //  Log that message
+            //  and return out of the method
+            if (geofencingEvent.hasError()) {
+                val errorMessage = errorMessage(context, geofencingEvent.errorCode)
+                Log.e(TAG, errorMessage)
+                return
+            }
+            // DONE: Step 11.4 Check if the geofenceTransition type is ENTER
+            if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                // DONE: Step 11.5 If the triggeringGeofences array is not empty, set the fenceID to the first geofence’s requestId.
+                //  We would only have one geofence active at a time, so if the array is non-empty then there would only be one for us to interact with.
+                //  If the array is empty,
+                //  log a message and return
+                if (geofencingEvent.triggeringGeofences.isNotEmpty()) {
+                    val fenceId = geofencingEvent.triggeringGeofences[0].requestId
+                    // DONE: Step 11.6 Check that the geofence is consistent with the constants listed in GeofenceUtil.kt.
+                    //  If not,
+                    //  print a log and return
+                    val foundIndex =
+                        GeofencingConstants.LANDMARK_DATA.indexOfFirst { it.id == fenceId }
+                    if (foundIndex == -1) {
+                        Log.e(TAG, "Unknown Geofence: Abort Mission")
+                        return
+                    }
+                    // DONE: Step 11.7 If your code has gotten this far, the user has found a valid geofence.
+                    //  Send a notification telling them the good news!
+                    val notificationManager =
+                        ContextCompat.getSystemService(
+                            context,
+                            NotificationManager::class.java
+                        ) as NotificationManager
+                    notificationManager.sendGeofenceEnteredNotification(context, foundIndex)
+                } else {
+                    Log.e(TAG, "No Geofence Trigger Found! Abort mission!")
+                    return
+                }
+            }
+        }
     }
 }
 
